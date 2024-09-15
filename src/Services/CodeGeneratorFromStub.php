78b856eacaafe4b747626fc93e628f17c9f8fbad
yassine-dabbous <yassine.dabbous@gmail.com>
2024-09-15 23:47:39 +0100

    support array values in stub generator

--- src/Services/CodeGeneratorFromStub.php
+++ src/Services/CodeGeneratorFromStub.php
@@ -6,20 +6,31 @@ use YassineDabbous\Generex\Protocols\StubData;
 
  class CodeGeneratorFromStub extends CodeGeneratorImp
 {
+    function valueToString($value): string {
+        if(is_array($value)){
+            $value = implode(', ', array_map(fn($v)=>"'$v'", array_values($value)));
+        }
+        return $value;
+    }
+
+
+
     public function renderContent(StubData $stub) : string {
         $this->dataHolder->__fillReplacements();
         return str_replace(
-            array_map(fn($v)=> '{'.$v.'}', array_keys($this->dataHolder->replacements)),
-            array_values($this->dataHolder->replacements), 
+            array_map(fn($v)=> "{{$v}}", array_keys($this->dataHolder->replacements)),
+            array_map(fn($v)=> $this->valueToString($v), array_values($this->dataHolder->replacements)),
             view($stub->source)
         );
     }
-    
+
+
+
     public function renderPath(string $path) : string {
         $this->dataHolder->__fillReplacements();
         return str_replace(
-            array_map(fn($v)=> '{'.$v.'}', array_keys($this->dataHolder->replacements)),
-            array_values($this->dataHolder->replacements), 
+            array_map(fn($v)=> "{{$v}}", array_keys($this->dataHolder->replacements)),
+            array_map(fn($v)=> $this->valueToString($v), array_values($this->dataHolder->replacements)),
             $path
         );
     }
